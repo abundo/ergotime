bbb7549b97299143ce766a687257a8d6c4694b35
Anders Lowinger <anders@abundo.se>
2016-08-08 20:02:22 +0200

    Removed basium from cx_freeze

--- client/build_release.bat
+++ client/build_release.bat
@@ -1,8 +1,9 @@
 rem set TARGETDIR=c:\opt\ergotime2-pyqt
+
 set PYTHON=c:\opt\python34
 set CXFREEZE=%PYTHON%\scripts\cxfreeze
-set PYTHONPATH=d:\hack\git\basium
+
 %PYTHON%\python build_release.py build
 
 mkdir build\exe.win-amd64-3.4\resource
-xcopy resource build\exe.win-amd64-3.4\resource /s
+xcopy resource build\exe.win-amd64-3.4\resource /s /y
