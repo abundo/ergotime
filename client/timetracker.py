184f497718ad126e733933f5740ab2b1328c48a2
Anders Lowinger <anders@abundo.se>
2020-01-15 21:11:30 +0100

    Avoid creating time object for 24 hours or longer

--- client/timetracker.py
+++ client/timetracker.py
@@ -150,6 +150,8 @@ class Timetracker(QtCore.QObject):
         self.reportmgr.store(self.report)
 
     def _seconds_to_time(self, seconds):
+        if seconds >= 86400:
+            seconds = 85399
         hour, remainder = int(seconds // 3600), int(seconds % 3600)
         minute, second = int(remainder // 60), int(remainder % 60)
         return datetime.time(hour, minute, second)
